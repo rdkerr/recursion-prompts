cd81c777b2817dafcd9906bfffd7f4e590c1a1cb
rdkerr <rogerdkerr@gmail.com>
2018-12-16 15:19:56 -0700

    Finished sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -62,6 +62,14 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+    if (n < 0) {
+        return 0 - sumBelow(0 - n);
+    }
+    else if (n <= 1) {
+        return 0;
+    } else {
+        return (n - 1) + sumBelow(n-1);
+    }
 };
 
 // 6. Get the integers within a range (x, y).
