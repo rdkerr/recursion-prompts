1a6ef9935da72fa92435cd24898af129b03670ca
rdkerr <rogerdkerr@gmail.com>
2018-12-17 11:09:47 -0700

    Finished alternateSign

--- src/recursion.js
+++ src/recursion.js
@@ -513,10 +513,20 @@ var minimizeZeroes = function(array) {
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
-// their original sign. The first number in the index always needs to be positive.
+// their original sign. The first number in th e index always needs to be positive.
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
 var alternateSign = function(array) {
+    if (array === undefined || array.length === 0) { 
+        return array; 
+    } 
+    if (array[0] < 0) { 
+        array[0] = -array[0]; 
+    }
+    if (array[1] > 0) { 
+        array[1] = -array[1]; 
+    }
+    return [array[0], array[1]].concat(alternateSign(array.slice(2)));
 };
 
 // 36. Given a string, return a string with digits converted to their word equivalent.
