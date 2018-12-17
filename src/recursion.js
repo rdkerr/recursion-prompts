f3892e27b864f3237b905dcc9f440a37fa4578b1
rdkerr <rogerdkerr@gmail.com>
2018-12-16 19:31:09 -0700

    Finished reverseArr

--- src/recursion.js
+++ src/recursion.js
@@ -232,6 +232,14 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+    var result = [];
+    if (array === undefined || array.length == 0) {
+        return result;
+    } else {
+        result.push(array[array.length - 1]);
+        return result.concat(reverseArr(array.slice(0,array.length - 1)));
+        
+    }
 };
 
 // 18. Create a new array with a given value and length.
