af87b8bad1efb902645928032139ff207d49f504
rdkerr <rogerdkerr@gmail.com>
2018-12-16 19:21:54 -0700

    Finished compareStr

--- src/recursion.js
+++ src/recursion.js
@@ -209,6 +209,13 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+    if (str1 === "" && str2 === "") {
+        return true;
+    } else if (str1 === "" || str2 === "") {
+        return false;
+    } else {
+        return str1[0] === str2[0] && compareStr(str1.slice(1),str2.slice(1));
+    }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
